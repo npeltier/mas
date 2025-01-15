32500012bf78f9af46dad98117f41311ab8d678e
Nicolas Peltier <npeltier@adobe.com>
2025-01-15 10:32:54 +0100

    adding if modified since mgt

--- io-actions/src/wcs/index.js
+++ io-actions/src/wcs/index.js
@@ -1,9 +1,22 @@
 async function main(params) {
+    if (
+        params?.__ow_headers?.['If-Modified-Since'] ||
+        params?.__ow_headers?.['if-modified-since']
+    ) {
+        return {
+            statusCode: 304,
+            headers: {
+                'Content-Type': 'application/json',
+                'Last-Modified': 'Mon, 08 Jan 2024 12:34:58 GMT',
+            },
+            body: '',
+        };
+    }
     return {
         statusCode: 200,
         headers: {
             'Content-Type': 'application/json',
-            'Last-Modified': 'Mon, 08 Jan 2024 12:34:56 GMT',
+            'Last-Modified': 'Mon, 08 Jan 2024 12:34:58 GMT',
         },
         body: '{"resolvedOffers":[]}',
     };
