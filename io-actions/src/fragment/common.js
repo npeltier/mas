6c14459c2596cacf10518680744077d2811312a0
Nicolas Peltier <npeltier@adobe.com>
2025-03-04 17:58:04 +0100

    MWPW-169073 log main steps,
    - centralise fetch usage, with log & error management,
    - centralise log management, with request id, transformer name,
    - add request id to context if not provided

---
+++ io-actions/src/fragment/common.js
@@ -0,0 +1,48 @@
+const fetch = require('node-fetch');
+
+function logPrefix(context, type = 'info') {
+    return `[${type}][${context.requestId}][${context.transformer}]`;
+}
+
+function log(message, context, type) {
+    console.log(`${logPrefix(context, type)} ${message}`);
+}
+
+async function getErrorMessage(response) {
+    let message = 'nok';
+    try {
+        const json = await response.json();
+        message = json?.detail;
+    } catch (e) {}
+    return message;
+}
+
+async function internalFetch(path, context) {
+    try {
+        const response = await fetch(path, {
+            headers: context.DEFAULT_HEADERS,
+        });
+        const success = response.status == 200;
+        const message = success ? 'ok' : await getErrorMessage(response);
+        log(
+            `fetch ${path} (${response?.status}) ${message}`,
+            context,
+            success ? 'info' : 'error',
+        );
+        return response;
+    } catch (e) {
+        console.error(
+            `${logPrefix(context, 'error')}[fetch] ${path} fetch error`,
+            e,
+        );
+    }
+    return {
+        status: 500,
+        message: 'fetch error',
+    };
+}
+
+module.exports = {
+    fetch: internalFetch,
+    log,
+};
